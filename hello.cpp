dc334043b9b5dbac35f79e47b613d0971b87cec1
akbarhossainalvee <akbarhossain69@protonmail.com>
2024-04-01 16:32:50 -0700

    First Commit

---
+++ hello.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define int long long
+#define lld long double
+#define ull unsigned long long
+#define size(x) (int) x.size()
+#define all(x) (x).begin(), (x).end() 
+
+void solve()
+{
+    cout<<"Hello"<<endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t = 1; 
+    // cin>>t; 
+    while(t--) solve();
+
+    return 0;
+}
